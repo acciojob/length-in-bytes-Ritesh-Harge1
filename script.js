9d9b5c09dcf5ba65c53dd8d0e7fced8656581d8c
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-21 16:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const byteSize = (str) => {
-  // write your code here
-};
+function byteSize(str) {
+  return Buffer.byteLength(str, 'utf8');
+}
 
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// Examples:
+console.log(byteSize("hello world")); // 11
+console.log(byteSize("안녕하세요"));   // 15
+console.log(byteSize(""));            // 0
